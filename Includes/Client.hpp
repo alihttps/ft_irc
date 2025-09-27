c0c9042983e953246a78b2a5289b627c13610569
Laabdi <chinwinouad04@gmail.com>
2025-09-27 16:59:37 +0100

    client necessary thing

--- Includes/Client.hpp
+++ Includes/Client.hpp
@@ -1,11 +1,30 @@
 #pragma once
+#include <string>
 
 class Client
 {
-    private:
-        // Private members will go here
-        
-    public:
-        Client();
-        ~Client();
+  private:
+	int Fd;
+	std::string Ipaddress;
+	// Private umembers will go here
+
+  public:
+      Client();
+      ~Client();
+	void set_fd(int fd)
+	{
+		this->Fd = fd;
+	}
+	int get_fd()
+	{
+		return (this->Fd);
+	}
+	void set_ipadd(std::string add)
+	{
+		this->Ipaddress = add;
+	}
+	std::string get_ipadd()
+	{
+		return (this->Ipaddress);
+	}
 };
