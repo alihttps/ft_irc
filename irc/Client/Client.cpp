f6688b9e4a3077ae74da134edec79190cf30abcb
Laabdi <chinwinouad04@gmail.com>
2025-09-28 18:24:48 +0100

    client necessary thing

--- irc/Client/Client.cpp
+++ irc/Client/Client.cpp
@@ -1,11 +1,20 @@
 #include "Client.hpp"
+#include "Channel.hpp"
 
-Client::Client()
-{
-    // Constructor implementation
-}
+// size_t C::space_count(string &line)
+// {
+// 	size_t	num;
 
-Client::~Client()
-{
-    // Destructor implementation
-}
+// 	num = count(line.begin(), line.end(), ' ');
+// 	return (num);
+// }
+
+// Client::Client()
+// {
+// 	// Constructor implementation
+// }
+
+// Client::~Client()
+// {
+// 	// Destructor implementation
+// }
--- irc/Client/Commands/JOIN.cpp
+++ irc/Client/Commands/JOIN.cpp
@@ -2,7 +2,48 @@
 
 // JOIN command implementation
 // This file will contain the JOIN command logic for IRC
+std::vector<std::string> ft_split_request(std::string &request,const std::string &delimiter)
+{
+	size_t	end;
+	size_t	start;
+	int		i;
 
+	std::vector<std::string> lines;
+	start = 0;
+	i = 0;
+	end = request.find(delimiter);
+	while (end != std::string::npos)
+	{
+		lines.push_back(request.substr(start, end - start));
+		// std::cout << "testing line number " << i << " :" << lines[i] << std::endl;
+		start = end + delimiter.length();
+		end = request.find(delimiter, start);
+		i++;
+	}
+	lines.push_back(request.substr(start));
+	return (lines);
+}
+int check_channel_name(std::string &c_name)
+{
+    if(c_name[0] == '#')
+	{
+		if(c_name.length() > 1)
+		return 1;
+	}
+	return 0;
+}
+
+int valid_syntax(std::string &request)
+{
+    std::vector<std::string> divided = ft_split_request(request, " ");
+    if(divided.size() < 2)
+        throw("error not enough parametres \n");
+    if(divided[0] == "JOIN")
+    {
+		if(check_channel_name(divided[0] = 1))
+
+    }
+}
 // void	addMember(int client_fd, const std::string &nick)
 // {
 
